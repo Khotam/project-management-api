3d86e733f0a71feda57454e6be04e9876a63218f
Bakhromov, Khotam <Khotam.Bakhromov@wolterskluwer.com>
2023-11-20 18:40:19 +0500

    feat: setup db and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { resolve } from 'path';
+import * as process from 'process';
+
+import config from '../config/config';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        logging: configService.get('database.logging'),
+        namingStrategy: new SnakeNamingStrategy(),
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        migrationsTableName: 'migrations',
+        migrationsRun: true,
+        maxQueryExecutionTime: 3000,
+        migrations: [resolve(__dirname, '../migrations/*.{ts,js}')],
+        migrationsTransactionMode: 'all',
+        autoLoadEntities: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env-${process.env.NODE_ENV?.trim() || 'dev'}`,
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+  const logger = new Logger('Project-Management-Api');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['debug'],
+  });
+
+  app.enableCors();
+
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  await app.listen(port, () => {
+    logger.log(`The [Project Management Api] service is up and running on ${port} port`);
+  });
 }
 bootstrap();
