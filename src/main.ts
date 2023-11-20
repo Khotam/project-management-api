683bc1f6f4f1c96746cb78d2d6d208a40adfa130
Bakhromov, Khotam <Khotam.Bakhromov@wolterskluwer.com>
2023-11-20 18:57:56 +0500

    feat: setup swagger and validation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import * as packageJson from '../package.json';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
+  const version = packageJson.version;
   const logger = new Logger('Project-Management-Api');
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['debug'],
   });
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
+  app.useGlobalFilters().useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('REST API')
+    .setDescription(`Project Management App REST API (v.${version})`)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
