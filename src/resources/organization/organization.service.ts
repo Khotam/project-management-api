cf7f3416a1396013a494034c0364d0fa01717f5f
Bakhromov, Khotam <Khotam.Bakhromov@wolterskluwer.com>
2023-11-22 16:26:46 +0500

    refactor: make service code  more readable by moving queries to its own file

--- src/resources/organization/organization.service.ts
+++ src/resources/organization/organization.service.ts
@@ -6,38 +6,42 @@ import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
 import { Organization } from './entities/organization.entity';
 import { FindAllResponse } from 'src/shared/types';
+import {
+  createDeleteQuery,
+  createInsertQuery,
+  createSelectAllQuery,
+  createSelectWhereQuery,
+  createUpdateQuery,
+} from './organization.query';
 
 @Injectable()
 export class OrganizationService {
   logger = new Logger(OrganizationService.name);
-  tableName = 'organizations';
 
   constructor(@InjectRepository(Organization) private orgsRepository: Repository<Organization>) {}
 
   async create(userId: number, { name }: CreateOrganizationDto) {
-    const [newOrg] = await this.orgsRepository.query(
-      `INSERT INTO ${this.tableName} (name, "createdBy") VALUES ($1, $2) RETURNING *`,
-      [name, userId],
-    );
+    const { query, queryParams } = createInsertQuery(name, userId);
+    const [newOrg] = await this.orgsRepository.query(query, queryParams);
     this.logger.log(`Created successfully`);
 
     return newOrg;
   }
 
   async findAll(): Promise<FindAllResponse<Organization>> {
-    const itemsPromise = this.orgsRepository.query(`SELECT * FROM ${this.tableName}`);
-    const countPromise = this.orgsRepository.query(`SELECT COUNT(*) FROM ${this.tableName}`);
-    const [items, [count]] = await Promise.all([itemsPromise, countPromise]);
+    const [selectQuery, countQuery] = createSelectAllQuery();
+    const [items, [count]] = await Promise.all([
+      this.orgsRepository.query(selectQuery),
+      this.orgsRepository.query(countQuery),
+    ]);
     this.logger.log(`Items count: ${count.count}`);
 
     return { items, count: count.count };
   }
 
   async findOne(id: number): Promise<Organization> {
-    const [item] = await this.orgsRepository.query(
-      `SELECT org.name, u.name as "createdBy" FROM ${this.tableName} as org JOIN "users" as u ON org."createdBy" = u.id WHERE org.id = $1`,
-      [id],
-    );
+    const { query, queryParams } = createSelectWhereQuery(id);
+    const [item] = await this.orgsRepository.query(query, queryParams);
     if (!item) {
       this.logger.debug('Item not found', id);
       throw new NotFoundException({ message: `Item with id: ${id} not found` });
@@ -48,13 +52,15 @@ export class OrganizationService {
 
   async update(id: number, { name }: UpdateOrganizationDto) {
     const org = await this.findOne(id);
-    await this.orgsRepository.query(`UPDATE ${this.tableName} SET name = $1 WHERE id = $2`, [name ?? org.name, org.id]);
+    const { query, queryParams } = createUpdateQuery(name, org);
+    await this.orgsRepository.query(query, queryParams);
     this.logger.log('Successfully updated');
   }
 
   async remove(id: number) {
     const org = await this.findOne(id);
-    await this.orgsRepository.query(`DELETE FROM ${this.tableName} WHERE id = $1`, [org.id]);
+    const { query, queryParams } = createDeleteQuery(org.id);
+    await this.orgsRepository.query(query, queryParams);
     this.logger.log('Successfully deleted');
   }
 }
