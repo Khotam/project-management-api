7ac9bac39ed86e46065bd5e8db17ad1c3de823ab
Khotam Bakhromov <bakhromov17@gmail.com>
2024-08-18 21:26:27 +0500

    fix env cd

--- config/config.ts
+++ config/config.ts
@@ -5,7 +5,7 @@ export default () => ({
   environment: process.env.NODE_ENV || 'dev',
   database: {
     logging: process.env.NODE_ENV === 'dev',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'localh',
     port: parseInt(process.env.DB_PORT) || 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
